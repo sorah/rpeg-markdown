10fc0491f53a3182f3d53cbfad6d8e73900861ac
Sora Harakami <sora134@gmail.com>
2010-07-13 16:15:18 +0900

    works in ruby-trunk (ruby 1.9.3dev (2010-07-12 trunk 28620) [x86_64-darwin10.4.0])

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,10 +4,7 @@ dir_config('peg_markdown')
 
 $objs = %w[markdown.o markdown_lib.o markdown_output.o markdown_parser.o]
 
-if pkg_config = find_executable('pkg-config')
-  $CFLAGS = `#{pkg_config} --cflags glib-2.0`
-  $LDFLAGS = `#{pkg_config} --libs glib-2.0`
-else
+unless pkg_config('glib-2.0')
   fail "glib2 not found"
 end
 
